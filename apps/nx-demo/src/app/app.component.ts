c2a62fb42b301c5238116c5e7a95efdf8327cc81
Sashikumar <yshashi30@gmail.com>
2024-03-10 14:41:45 +0800

    Update project configuration and files

--- apps/nx-demo/src/app/app.component.ts
+++ apps/nx-demo/src/app/app.component.ts
@@ -1,11 +1,12 @@
+import { ButtonComponent, HeaderComponent } from '@ng-workspace/ui';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'ng-workspace-root',
+  imports: [NxWelcomeComponent, RouterModule, ButtonComponent, HeaderComponent],
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
